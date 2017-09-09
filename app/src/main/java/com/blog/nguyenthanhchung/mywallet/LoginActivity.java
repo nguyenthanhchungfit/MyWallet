b0f3efd3e696cd11a13954e5646bded19aced16c
ThanhChung <nguyenthanhchungfit@gmail.com>
2017-09-09 21:27:25 +0700

    // add custom_login_action_ bar and edit it

--- app/src/main/java/com/blog/nguyenthanhchung/mywallet/LoginActivity.java
+++ app/src/main/java/com/blog/nguyenthanhchung/mywallet/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.blog.nguyenthanhchung.mywallet;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.ViewGroup;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.custom_login_action_bar);
+        ActionBar.LayoutParams p = new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        p.gravity = Gravity.CENTER;
     }
 }
